2b3b817452c67b8e504e8116528f2aba900f87ef
LordNature <lordnature@protonmail.com>
2018-11-24 03:47:10 -0600

    Added profile router

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-
+# Home Routing
 @app.route('/')
 def home():
-    return "Hello World!"
+	return "Hello World!"
+
+# Profile Placeholder
+@app.route('/<id>')
+def hello_name(name):
+	return "Your id is {}".format(id)
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
