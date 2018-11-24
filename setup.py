9f37cb628e58d373905e63cc6719a8e3e4e190c9
LordNature <lordnature@protonmail.com>
2018-11-24 03:30:19 -0600

    Skeleton framework

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='birdcraft',
+	version='0.1',
+	long_description='birdcraft is a social website.',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=['Flask', 'requests']
+)
