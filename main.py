01c29cd4b697c92719b87403ce3bab49695ea6e7
LordNature <lordnature@protonmail.com>
2018-11-24 04:07:12 -0600

    Layout added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Home Routing
 @app.route('/')
-def index():
+def home():
 	return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -11,8 +11,8 @@ def login():
 	return "In development"
 
 # Profile Placeholder
-@app.route('/<id>')
-def hello_name(name):
+@app.route('/u<id>')
+def profile(id):
 	return "Your id is {}".format(id)
 
 if __name__ == '__main__':
