ef812747947a9a64b329456b2b86f660e624300a
LordNature <lordnature@protonmail.com>
2018-11-24 04:20:02 -0600

    Login routing

--- main.py
+++ main.py
@@ -6,9 +6,24 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
+# Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	return "In development"
+	if request.method == "POST":
+		usr = request.form['usr']
+		pwd = request.form['pwd']
+		error = None
+
+		if not usr:
+			error = "Username required."
+		elif not pwd:
+			error = "Password required."
+
+		if error is None:
+			return "worked"
+
+		flash(error)
+	return render_template('login.html')
 
 # Profile Placeholder
 @app.route('/u<id>')
